3fbba6f7e62cf4f472a07fa6321fe5c48b894e58
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-07-09 17:04:10 +0900

    feat: dog

--- src/pages/Quarcism.tsx
+++ src/pages/Quarcism.tsx
@@ -22,12 +22,21 @@ export default function QuarcismPage() {
       <Seo />
       <section className='relative bg-black'>
         <div className='inline-flexflex mr-4 min-h-screen flex-col p-4 text-left'>
-          <h1 className='tracking-wide text-white'>쿼시즘(Quarcism)</h1>
+          <h1 className='tracking-wide text-white'>
+            쿼시즘(Quarcism) - 개소리의 시작
+          </h1>
           <div className='relative flex items-center py-5'>
             <div className='flex-grow border-t border-white'></div>
           </div>
           <p className='text-xl text-white'>
-            다른 이에게 피해를 주지 않는 한에서 최대한의 자유.
+            &quot;편한대로 하고 싶은거 마음대로 하면서 사세요. 단, 다른 사람도
+            이와 같은 권리가 있습니다.&quot;
+            <br />
+            <br />
+            개개인이 하고 싶은 일을 찾아, 그 부분만을 발전시켜 서로가 공유해낼
+            수 있다면, 그 개개인들이 모인 집단의 발전이 이루어집니다.
+            <br />
+            <br />
           </p>
         </div>
       </section>
